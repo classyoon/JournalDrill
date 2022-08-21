fc47bce269b4388e0cc8b9035c7d4ebad0905b5f
Conner Yoon <con.yoon@icloud.com>
2022-08-21 17:38:56 -0400

    Initial Commit

---
+++ JournalDrill/JournalDrillApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalDrillApp.swift
+//  JournalDrill
+//
+//  Created by Conner Yoon on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct JournalDrillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
